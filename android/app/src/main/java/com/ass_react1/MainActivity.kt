d71ce5347f24331119485a3c0e5065a29afecff0
kiendtph37589 <dinhtrongkien2004@gmail.com>
2024-10-10 22:00:11 +0700

    abc

--- android/app/src/main/java/com/ass_react1/MainActivity.kt
+++ android/app/src/main/java/com/ass_react1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ass_react1
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +13,9 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "Ass_react1"
+  override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(null)
+    }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
